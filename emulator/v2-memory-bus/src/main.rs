1e5a8db5f886de9a0f45a3b95ecbe32bc47caab4
siriusdemon <mory2016@126.com>
2022-06-01 10:27:32 +0800

    remove println

--- emulator/v2-memory-bus/src/main.rs
+++ emulator/v2-memory-bus/src/main.rs
@@ -43,7 +43,6 @@ fn main() -> io::Result<()> {
                 break;
             }
         };
-        println!("inst: {:#x}" , inst);
         match cpu.execute(inst) {
             Ok(_) => (),
             Err(e) => {
