175692900f0ad2d3b7aa98743f61665f059fa656
siriusdemon <mory2016@126.com>
2022-06-27 18:02:22 +0800

    v6 refactor

--- emulator/v6-plic-clint/src/bus.rs
+++ emulator/v6-plic-clint/src/bus.rs
@@ -1,8 +1,10 @@
+//! The bus module contains the system bus which can access the memroy or memory-mapped peripheral
+//! devices.
 use crate::param::*;
 use crate::dram::Dram;
 use crate::plic::Plic;
 use crate::clint::Clint;
-use crate::exception::RvException;
+use crate::exception::*;
 
 pub struct Bus {
     dram: Dram,
@@ -13,28 +15,28 @@ pub struct Bus {
 
 // Bus is used to transfer data, so check data access size here is appropriate
 impl Bus {
-    pub fn new(code: Vec<u8>) -> Bus {
+    pub fn new(code: Vec<u8>, disk_image: Vec<u8>) -> Bus {
         Self { 
             dram: Dram::new(code),
             clint: Clint::new(),
             plic: Plic::new(),
         }
     }
-    pub fn load(&self, addr: u64, size: u64) -> Result<u64, RvException> {
+    pub fn load(&mut self, addr: u64, size: u64) -> Result<u64, Exception> {
         match addr {
             CLINT_BASE..=CLINT_END => self.clint.load(addr, size),
             PLIC_BASE..=PLIC_END => self.plic.load(addr, size),
             DRAM_BASE..=DRAM_END => self.dram.load(addr, size),
-            _ => Err(RvException::LoadAccessFault(addr)),
+            _ => Err(Exception::LoadAccessFault(addr)),
         }
     }
 
-    pub fn store(&mut self, addr: u64, size: u64, value: u64) -> Result<(), RvException> {
+    pub fn store(&mut self, addr: u64, size: u64, value: u64) -> Result<(), Exception> {
         match addr {
             CLINT_BASE..=CLINT_END => self.clint.store(addr, size, value),
             PLIC_BASE..=PLIC_END => self.plic.store(addr, size, value),
             DRAM_BASE..=DRAM_END => self.dram.store(addr, size, value),
-            _ => Err(RvException::StoreOrAMOAccessFault(addr)),
+            _ => Err(Exception::StoreAMOAccessFault(addr)),
         }
     }
 }
--- emulator/v6-plic-clint/src/clint.rs
+++ emulator/v6-plic-clint/src/clint.rs
@@ -1,7 +1,12 @@
-use crate::param::{CLINT_BASE};
-use crate::exception::RvException;
+//! The clint module contains the core-local interruptor (CLINT). The CLINT
+//! block holds memory-mapped control and status registers associated with
+//! software and timer interrupts. It generates per-hart software interrupts and timer.
 
-use RvException::*;
+use crate::bus::*;
+use crate::exception::*;
+use crate::param::*;
+
+use Exception::*;
 
 pub struct Clint {
     mtime: u64,
@@ -13,7 +18,7 @@ impl Clint {
         Self { mtime: 0, mtimecmp: 0 }
     }
     
-    pub fn load(&self, addr: u64, size: u64) -> Result<u64, RvException> {
+    pub fn load(&self, addr: u64, size: u64) -> Result<u64, Exception> {
         if size != 64 {
             return Err(LoadAccessFault(addr));
         }
@@ -24,14 +29,14 @@ impl Clint {
         }
     }
 
-    pub fn store(&mut self, addr: u64, size: u64, value: u64) -> Result<(), RvException> {
+    pub fn store(&mut self, addr: u64, size: u64, value: u64) -> Result<(), Exception> {
         if size != 64 {
             return Err(LoadAccessFault(addr));
         }
         match addr {
             CLINT_MTIMECMP => Ok(self.mtimecmp = value),
             CLINT_MTIME => Ok(self.mtime = value),
-            _ => Err(StoreOrAMOAccessFault(addr)),
+            _ => Err(StoreAMOAccessFault(addr)),
         }
     }
 
