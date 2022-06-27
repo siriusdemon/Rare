393c9dbb5b71199c2bfb61a8286a4dbae34c4a7a
siriusdemon <mory2016@126.com>
2022-06-27 18:23:22 +0800

    v2 refactor

--- emulator/v2-memory-bus/src/bus.rs
+++ emulator/v2-memory-bus/src/bus.rs
@@ -1,37 +1,32 @@
-use crate::{DRAM_BASE, DRAM_END};
+//! The bus module contains the system bus which can access the memroy or memory-mapped peripheral
+//! devices.
+use crate::param::*;
 use crate::dram::Dram;
-use crate::exception::RvException;
+use crate::exception::*;
 
 pub struct Bus {
     dram: Dram,
 }
 
-// Bus is used to transfer data, so check data access size here is appropriate
-pub fn valid_size(size: u64) -> bool {
-    [8, 16, 32, 64].contains(&size)
-}
 
+// Bus is used to transfer data, so check data access size here is appropriate
 impl Bus {
-    pub fn new(code: Vec<u8>) -> Bus {
-        Self { dram: Dram::new(code) }
-    }
-    pub fn load(&self, addr: u64, size: u64) -> Result<u64, RvException> {
-        if !valid_size(size) {
-            return Err(RvException::InvalidSize(size));
+    pub fn new(code: Vec<u8>, disk_image: Vec<u8>) -> Bus {
+        Self { 
+            dram: Dram::new(code),
         }
+    }
+    pub fn load(&mut self, addr: u64, size: u64) -> Result<u64, Exception> {
         match addr {
             DRAM_BASE..=DRAM_END => self.dram.load(addr, size),
-            _ => Err(RvException::InvalidAddress(addr)),
+            _ => Err(Exception::LoadAccessFault(addr)),
         }
     }
 
-    pub fn store(&mut self, addr: u64, size: u64, value: u64) -> Result<(), RvException> {
-        if !valid_size(size) {
-            return Err(RvException::InvalidSize(size));
-        }
+    pub fn store(&mut self, addr: u64, size: u64, value: u64) -> Result<(), Exception> {
         match addr {
             DRAM_BASE..=DRAM_END => self.dram.store(addr, size, value),
-            _ => Err(RvException::InvalidAddress(addr)),
+            _ => Err(Exception::StoreAMOAccessFault(addr)),
         }
     }
 }
--- emulator/v2-memory-bus/src/exception.rs
+++ emulator/v2-memory-bus/src/exception.rs
@@ -1,19 +1,109 @@
 use std::fmt;
 
-#[derive(Debug)]
-pub enum RvException {
-    InvalidAddress(u64),
-    InvalidSize(u64),
-    InvalidInstruction(u64),
+#[derive(Debug, Copy, Clone)]
+pub enum Exception {
+    // Riscv Standard Exception
+    InstructionAddrMisaligned(u64),
+    InstructionAccessFault(u64),
+    IllegalInstruction(u64),
+    Breakpoint(u64),
+    LoadAccessMisaligned(u64),
+    LoadAccessFault(u64),
+    StoreAMOAddrMisaligned(u64),
+    StoreAMOAccessFault(u64),
+    EnvironmentCallFromUMode(u64),
+    EnvironmentCallFromSMode(u64),
+    EnvironmentCallFromMMode(u64),
+    InstructionPageFault(u64),
+    LoadPageFault(u64),
+    StoreAMOPageFault(u64),
 }
 
-impl fmt::Display for RvException {
+use Exception::*;
+impl fmt::Display for Exception {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        use RvException::*;
         match self {
-            InvalidAddress(addr) => write!(f, "Invalid Address {:#x}", addr),
-            InvalidSize(size) => write!(f, "Invalid size {}", size),
-            InvalidInstruction(inst) => write!(f, "Invalid instruction {:#x}", inst),
+            InstructionAddrMisaligned(addr) => write!(f, "Instruction address misaligned {:#x}", addr),
+            InstructionAccessFault(addr) => write!(f, "Instruction access fault {:#x}", addr),
+            IllegalInstruction(inst) => write!(f, "Illegal instruction {:#x}", inst),
+            Breakpoint(pc) => write!(f, "Breakpoint {:#x}", pc),
+            LoadAccessMisaligned(addr) => write!(f, "Load access {:#x}", addr),
+            LoadAccessFault(addr) => write!(f, "Load access fault {:#x}", addr),
+            StoreAMOAddrMisaligned(addr) => write!(f, "Store or AMO address misaliged {:#x}", addr),
+            StoreAMOAccessFault(addr) => write!(f, "Store or AMO access fault {:#x}", addr),
+            EnvironmentCallFromUMode(pc) => write!(f, "Environment call from U-mode {:#x}", pc),
+            EnvironmentCallFromSMode(pc) => write!(f, "Environment call from S-mode {:#x}", pc),
+            EnvironmentCallFromMMode(pc) => write!(f, "Environment call from M-mode {:#x}", pc),
+            InstructionPageFault(addr) => write!(f, "Instruction page fault {:#x}", addr),
+            LoadPageFault(addr) => write!(f, "Load page fault {:#x}", addr),
+            StoreAMOPageFault(addr) => write!(f, "Store or AMO page fault {:#x}", addr),
         }
+    }
+}
+
+
+impl Exception {
+    pub fn value(self) -> u64 {
+        match self {
+            InstructionAddrMisaligned(addr) => addr,
+            InstructionAccessFault(addr) => addr,
+            IllegalInstruction(inst) => inst,
+            Breakpoint(pc) => pc,
+            LoadAccessMisaligned(addr) => addr,
+            LoadAccessFault(addr) => addr,
+            StoreAMOAddrMisaligned(addr) => addr,
+            StoreAMOAccessFault(addr) => addr,
+            EnvironmentCallFromUMode(pc) => pc,
+            EnvironmentCallFromSMode(pc) => pc,
+            EnvironmentCallFromMMode(pc) => pc,
+            InstructionPageFault(addr) => addr,
+            LoadPageFault(addr) => addr,
+            StoreAMOPageFault(addr) => addr,
+        }
+    }
+
+    pub fn code(self) -> u64 {
+        match self {
+            InstructionAddrMisaligned(_) => 0,
+            InstructionAccessFault(_) => 1,
+            IllegalInstruction(_) => 2,
+            Breakpoint(_) => 3,
+            LoadAccessMisaligned(_) => 4,
+            LoadAccessFault(_) => 5,
+            StoreAMOAddrMisaligned(_) => 6,
+            StoreAMOAccessFault(_) => 7,
+            EnvironmentCallFromUMode(_) => 8,
+            EnvironmentCallFromSMode(_) => 9,
+            EnvironmentCallFromMMode(_) => 11,
+            InstructionPageFault(_) => 12,
+            LoadPageFault(_) => 13,
+            StoreAMOPageFault(_) => 15,
+        }
+    }
+
+    pub fn is_fatal(self) -> bool {
+        match self {
+            InstructionAddrMisaligned(_)
+            | InstructionAccessFault(_)
+            | LoadAccessFault(_)
+            | StoreAMOAddrMisaligned(_)
+            | StoreAMOAccessFault(_) 
+            | IllegalInstruction(_) => true,
+            _else => false,
+        }
+    }
+}
+
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_code() {
+        let e = Exception::IllegalInstruction(0x0);
+        assert_eq!(e.value(), 0);
+        assert_eq!(e.code(), 2);
     }
 }
