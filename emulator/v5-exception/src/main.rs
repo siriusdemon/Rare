33e38440b4c0ed16f9f1ad60226f64388291eb62
siriusdemon <mory2016@126.com>
2022-06-27 18:08:32 +0800

    v5 refactor

--- emulator/v5-exception/src/main.rs
+++ emulator/v5-exception/src/main.rs
@@ -1,67 +1,68 @@
+mod bus;
+mod cpu;
+mod dram;
+mod param;
+mod csr;
+mod exception;
+
 use std::env;
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 
-
-mod param;
-mod dram;
-mod cpu;
-mod bus;
-mod exception;
-mod csr;
-
-pub use param::*;
-use cpu::Cpu;
-
-
-const ITERATION: usize = 10000;
-
+use crate::cpu::*;
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 2 {
-        println!(
-            "Usage:\n\
-            - rvemu <filename>\n\
-            - cargo run <filename>"
-        );
-        return Ok(());
+    if (args.len() != 2) && (args.len() != 3) {
+        panic!("Usage: rvemu-for-book <filename> <(option) image>");
+    }
+    let mut file = File::open(&args[1])?;
+    let mut binary = Vec::new();
+    file.read_to_end(&mut binary)?;
+
+    let mut disk_image = Vec::new();
+    if args.len() == 3 {
+        let mut file = File::open(&args[2])?;
+        file.read_to_end(&mut disk_image)?;
     }
 
-    let mut file = File::open(&args[1])?;
-    let mut code = Vec::new();
-    file.read_to_end(&mut code)?;
+    let mut cpu = Cpu::new(binary, disk_image);
 
-    let mut cpu = Cpu::new(code);
-
-    for _i in 0..ITERATION {
+    loop {
+        // 1. Fetch.
         let inst = match cpu.fetch() {
+            // Break the loop if an error occurs.
             Ok(inst) => inst,
-            Err(e) => { 
-                cpu.handle_exception(e); 
-                if e.is_fatal() {
-                    println!("Riscv exception: {}", e);
-                    break;
-                }
-                continue;
-            }
-        };
-        match cpu.execute(inst) {
-            Ok(_) => (),
             Err(e) => {
                 cpu.handle_exception(e);
                 if e.is_fatal() {
-                    println!("Riscv exception: {}", e);
                     break;
                 }
                 continue;
             }
         };
+
+        // 2. Add 4 to the program counter.
+        // cpu.pc += 4;
+        // 3. Decode.
+        // 4. Execute.
+        match cpu.execute(inst) {
+            // Break the loop if an error occurs.
+            Ok(new_pc) => cpu.pc = new_pc,
+            Err(e) => {
+                cpu.handle_exception(e);
+                if e.is_fatal() {
+                    break;
+                }
+            }
+        };
+
     }
     cpu.dump_registers();
-    cpu.csr.dump_csrs();
-    cpu.dump_pc();
+    cpu.dump_csrs();
+    // cpu.dump_pc();
+
     Ok(())
 }
